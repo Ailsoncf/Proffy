fe57afdde24f1c17d7e8a133c0426bb826f125c4
ailsoncf <ailson_cf@yahoo.com.br>
2020-08-14 15:14:28 +1200

    First Commit

---
+++ backend/src/database/migrations/01_create_classes.js
@@ -0,0 +1,17 @@
+exports.up = async function(knex){
+    return knex.schema.createTable('classes', table => {
+        table.increments('id').primary();
+        table.string('subject').notNullable();
+        table.decimal('cost').notNullable();
+
+        table.integer('uses_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+      })
+}
+exports.down = async function(knex){
+    return knex.schema.dropTable('classes')
+}
