f59d320a8ef2da7f0a64c4b98cfd596a716d49a9
ailsoncf <ailson_cf@yahoo.com.br>
2020-08-14 17:23:08 +1200

    Creating Classes Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,50 +1,9 @@
 const {Router} = require('express')
 const db = require('./database/connection')
-const convertTimeInMinutes = require('./utils/convertHoursToMinutes')
-const convertHoursToMinutes = require('./utils/convertHoursToMinutes')
+const ClassesController = require('./controllers/ClassesController')
 
 const routes = Router()
 
-routes.post('/classes', async (request, response) =>{
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body
-
-    const insertedUsersIds =  await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio
-    })
-
-    const user_id = insertedUsersIds[0]
-
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id
-    })
-
-    const class_id = insertedClassesIds[0]
-
-    const classSchedule = schedule.map(scheduleItem => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHoursToMinutes(scheduleItem.from),
-            to: convertHoursToMinutes(scheduleItem.to)
-        }
-    })
-
-    await db('class_schedule').insert(classSchedule)
-
-    return response.send()
-})
+routes.post('/classes', ClassesController.create)
 
 module.exports = routes
