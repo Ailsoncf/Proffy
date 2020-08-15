48a0b13dcab076462388d4465f6b6587e7be38db
ailsoncf <ailson_cf@yahoo.com.br>
2020-08-15 18:07:21 +1200

    Create Landing Page

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import "./App.css";
+import "./assets/styles/global.css";
+
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello, Doidao</h1>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ web/src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import TeacherForm from "./pages/TeacherForm";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Landing} />
+      <Route path="give-classes" exact component={TeacherForm} />
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
