54ce41be402a043d24b52a5d45633674461a5e03
ailsoncf <ailson_cf@yahoo.com.br>
2020-08-17 13:42:03 +1200

    Create THeader Component

--- web/src/pages/TeacherForm/index.js
+++ web/src/pages/TeacherForm/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import PageHeader from "../../components/PageHeader";
+
 function TeacherForm() {
-  return <h1>Teacher form</h1>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer der aulas." />
+    </div>
+  );
 }
 
 export default TeacherForm;
--- web/src/routes.js
+++ web/src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import TeacherForm from "./pages/TeacherForm";
+import TeacherList from "./pages/TeacherList";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
-      <Route path="give-classes" exact component={TeacherForm} />
+      <Route path="/give-classes" component={TeacherForm} />
+      <Route path="/Study" component={TeacherList} />
     </BrowserRouter>
   );
 }
